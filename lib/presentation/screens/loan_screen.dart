e2fd57c5d9b6be89dfddf13600577b2bca4fc709
joaonotfound <joaonotfound@protonmail.com>
2023-01-27 19:19:20 -0300

    create main screens

---
+++ lib/presentation/screens/loan_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoanScreenWidget extends StatefulWidget {
+  const LoanScreenWidget({super.key});
+
+  @override
+  State<LoanScreenWidget> createState() => _LoanScreenWidgetState();
+}
+
+class _LoanScreenWidgetState extends State<LoanScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export './loan_screen.dart';
+export './stock_screen.dart';
