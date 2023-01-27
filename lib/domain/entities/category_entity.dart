6448a7f99a71b0d578b4db91da3c62719f42a284
joaonotfound <joaonotfound@protonmail.com>
2023-01-27 20:50:48 -0300

    fix wrong type on category_entity property

--- lib/domain/entities/category_entity.dart
+++ lib/domain/entities/category_entity.dart
@@ -2,7 +2,7 @@ import 'package:service_desk_2/domain/domain.dart';
 
 class CategoryEntity {
   final String name;
-  final List<StockItemEntity> models;
+  final List<StockItemModelEntity> models;
   const CategoryEntity({
     required this.name,
     required this.models,
