6c1285385edebc3dec8987888011eaf0cc0fca27
joaonotfound <joaonotfound@protonmail.com>
2023-01-27 19:25:06 -0300

    create stock_item_model class

---
+++ lib/domain/models/stock_item_model.dart
@@ -0,0 +1,14 @@
+class StockItemModel {
+  final String description;
+  final String model;
+  final String notes;
+  final String imageUrl;
+  final String receiptUrl;
+  const StockItemModel({
+    required this.description,
+    required this.model,
+    required this.notes,
+    required this.imageUrl,
+    required this.receiptUrl,
+  });
+}
