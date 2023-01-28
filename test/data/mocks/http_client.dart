c98b74b63c10505a0c54fc4fca886e1a5888b866
joaonotfound <joaonotfound@protonmail.com>
2023-01-28 11:49:07 -0300

    add mock http client post method

--- test/data/mocks/http_client.dart
+++ test/data/mocks/http_client.dart
@@ -8,4 +8,12 @@ class MockHttpClient extends Mock implements HttpClient {
       // ignore: prefer_const_constructors
       _mockGetCall().thenAnswer((_) async => response);
   void mockGetError(HttpError error) => _mockGetCall().thenThrow(error);
+
+  When _mockPostCall() =>
+      // ignore: unnecessary_this
+      when(() => this.post(url: any(named: "url"), body: any(named: "body")));
+  void mockPost(HttpResponse response) =>
+      // ignore: prefer_const_constructors
+      _mockPostCall().thenAnswer((_) async => response);
+  void mockPostError(HttpError error) => _mockGetCall().thenThrow(error);
 }
