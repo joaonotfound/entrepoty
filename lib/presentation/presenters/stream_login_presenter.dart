cd51822ce09386a29455e4473a622ab63cf22473
joaonotfound <joaonotfound@protonmail.com>
2023-01-28 15:55:16 -0300

    create a file to stream_login_presenter class

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:service_desk_2/ui/screens/screens.dart';
+
+class StreamLoginPresenter extends LoginPresenter {
+  final _idErrorController = StreamController<String>.broadcast();
+  final _passwordErrorController = StreamController<String>.broadcast();
+  final _isFormValidController = StreamController<bool>.broadcast();
+  final _isLoadingController = StreamController<bool>.broadcast();
+  final _mainErrorController = StreamController<String>.broadcast();
+  @override
+  Stream<String> get idErrorStream => _idErrorController.stream;
+
+  @override
+  Stream<String> get passwordErrorStream => _passwordErrorController.stream;
+
+  @override
+  Stream<bool> get isFormValidStream => _isFormValidController.stream;
+
+  @override
+  Stream<bool> get isLoadingStream => _isLoadingController.stream;
+  @override
+  Stream<String> get mainErrorStream => _mainErrorController.stream;
+
+  @override
+  void validateId(String id) {}
+
+  @override
+  void validatePassword(String password) {}
+
+  @override
+  void authenticate() {
+    _isLoadingController.add(true);
+  }
+
+  @override
+  void dispose() {}
+}
--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,43 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:service_desk_2/presentation/presenters/stream_login_presenter.dart';
 import 'package:service_desk_2/ui/screens/screens.dart';
-// ignore: depend_on_referenced_packages
-
-class StreamLoginPresenter extends LoginPresenter {
-  final _idErrorController = StreamController<String>.broadcast();
-  final _passwordErrorController = StreamController<String>.broadcast();
-  final _isFormValidController = StreamController<bool>.broadcast();
-  final _isLoadingController = StreamController<bool>.broadcast();
-  final _mainErrorController = StreamController<String>.broadcast();
-  @override
-  Stream<String> get idErrorStream => _idErrorController.stream;
-
-  @override
-  Stream<String> get passwordErrorStream => _passwordErrorController.stream;
-
-  @override
-  Stream<bool> get isFormValidStream => _isFormValidController.stream;
-
-  @override
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
-  @override
-  Stream<String> get mainErrorStream => _mainErrorController.stream;
-
-  @override
-  void validateId(String id) {}
-
-  @override
-  void validatePassword(String password) {}
-
-  @override
-  void authenticate() {
-    _isLoadingController.add(true);
-  }
-
-  @override
-  void dispose() {}
-}
 
 LoginPresenter makeLoginPresenter() {
   return StreamLoginPresenter();
