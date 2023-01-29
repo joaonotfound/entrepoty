5bdc302fc643844c4b8db05a54b4423c1bfda16a
joaonotfound <joaonotfound@protonmail.com>
2023-01-29 17:05:27 -0300

    remove unnecessary method

--- lib/domain/usecases/authentication/save_current_account_usecase.dart
+++ lib/domain/usecases/authentication/save_current_account_usecase.dart
@@ -1,8 +1,7 @@
-import 'package:service_desk_2/domain/domain.dart';
+import '../../../domain/domain.dart';
 
 abstract class SaveCurrentAccountUsecase {
   Future<void> saveAccount({
     required Account account,
   });
-  Future<Account?> loadCurrentAccount();
 }
