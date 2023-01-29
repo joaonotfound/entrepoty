d15e92ef81d9b7411b1ede1b9d58e4a2f5e8a336
joaonotfound <joaonotfound@protonmail.com>
2023-01-29 17:48:57 -0300

    create LocalAuthentication

--- lib/data/usecases/authentication/authentication.dart
+++ lib/data/usecases/authentication/authentication.dart
@@ -1,3 +1,4 @@
 export './remote_authentication.dart';
 export '../local_save_current_account.dart';
 export './remote_authentication.dart';
+export './local_authentication.dart';
---
+++ lib/data/usecases/authentication/local_authentication.dart
@@ -0,0 +1,19 @@
+import 'package:service_desk_2/domain/domain.dart';
+
+// only to be used while there's no backend creayed yet.
+class LocalAuthentication implements AuthenticationUsecase {
+  @override
+  Future<Account> authenticate({
+    required String id,
+    required String password,
+  }) async {
+    if (id == "ja-12345-6" && password == "123456") {
+      return Account(
+        id: id,
+        name: "João",
+        profilePictureUrl: "https://google.com",
+      );
+    }
+    throw DomainError.invalidCredentials;
+  }
+}
