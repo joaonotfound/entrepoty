d34d150f5137fea25d6f1da32519b66a3166cfb4
joaonotfound <joaonotfound@protonmail.com>
2023-01-29 17:52:08 -0300

    remove mock

--- lib/main/factories/usecases/http_factory.dart
+++ lib/main/factories/usecases/http_factory.dart
@@ -2,18 +2,8 @@ import 'package:mocktail/mocktail.dart';
 
 import '../../../data/data.dart';
 
-//todo: implements httpclient
 class MockHttpClient extends Mock implements HttpClient {}
 
 HttpClient makeHttpAdapter() {
-  var mockHttpClient = MockHttpClient();
-  when(() =>
-          mockHttpClient.post(url: any(named: "url"), body: any(named: "body")))
-      .thenAnswer(
-    (_) async {
-      await Future.delayed(const Duration(seconds: 2), () {});
-      throw HttpError.serverError;
-    },
-  );
-  return mockHttpClient;
+  return MockHttpClient();
 }
