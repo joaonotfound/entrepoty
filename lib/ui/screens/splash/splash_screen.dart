bbcaaf8737071d4d0c3f0a86b33f0b6a8f414fc0
joaonotfound <joaonotfound@protonmail.com>
2023-02-02 19:26:11 -0300

    change splash screen style

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -13,13 +13,25 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     presenter.checkAccount();
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       body: Builder(builder: (context) {
         presenter.navigateToStream.listen((page) {
           if (page?.isNotEmpty == true) {
             Get.offAllNamed(page!);
           }
         });
-        return Center(child: CircularProgressIndicator());
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                color: Colors.white,
+              ),
+              SizedBox(height: 20),
+              Text("Loading", style: TextStyle(color: Colors.white))
+            ],
+          ),
+        );
       }),
     );
   }
