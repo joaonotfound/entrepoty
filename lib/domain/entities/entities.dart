7c03aca6afa0f65ea9b0a090069b16cc8b1aa4ea
joaonotfound <joaonotfound@protonmail.com>
2023-02-02 21:29:59 -0300

    create UserEntity

--- lib/domain/entities/entities.dart
+++ lib/domain/entities/entities.dart
@@ -4,3 +4,4 @@ export './stock_item_model_entity.dart';
 export './category_entity.dart';
 export './authentication/authentication.dart';
 export './create_stock_item_entity.dart';
+export './user_entity.dart';
---
+++ lib/domain/entities/user_entity.dart
@@ -0,0 +1,8 @@
+class UserEntity {
+  final String name;
+  final String profilePicture;
+  const UserEntity({
+    required this.name,
+    required this.profilePicture,
+  });
+}
