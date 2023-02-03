9b81972391700d30894e5e9feab0f1d43d018030
joaonotfound <joaonotfound@protonmail.com>
2023-02-02 21:31:17 -0300

    create LoadUsersUsecase abstract class

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -4,3 +4,4 @@ export './save_current_account_usecase.dart';
 export './load_current_account_usecase.dart';
 export './load_stock_items.dart';
 export 'register_lot.dart';
+export './load_users_usecase.dart';
