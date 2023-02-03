fb2727240299dd8befaf922b772c00cf5a8b9de5
joaonotfound <joaonotfound@protonmail.com>
2023-02-02 22:07:21 -0300

    create MockLoadUsers

---
+++ test/mocks/load_users_mock.dart
@@ -0,0 +1,10 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:service_desk_2/domain/domain.dart';
+
+class MockLoadUsers extends Mock implements LoadUsersUsecase {
+  When _mockLoadUsersCall() => when(
+        () => loadUsers(),
+      );
+  void mockLoadUsers(List<UserEntity> data) =>
+      _mockLoadUsersCall().thenAnswer((invocation) async => data);
+}
--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -9,3 +9,4 @@ export './load_secure_cache_storage.dart';
 export './load_current_account_mock.dart';
 export './load_categories_mock.dart';
 export './load_stock_items.dart';
+export './load_users_mock.dart';
