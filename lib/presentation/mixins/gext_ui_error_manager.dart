334313d3862ed78ed5eab037de49bb92d146f9fd
joaonotfound <joaonotfound@protonmail.com>
2023-03-22 20:10:58 -0300

    fix: allow distincts values on GetxUiErrorManager

--- lib/presentation/mixins/gext_ui_error_manager.dart
+++ lib/presentation/mixins/gext_ui_error_manager.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
+import 'package:entrepoty/ui/ui.dart';
 import 'package:get/get.dart';
 
 mixin GetxUiErrorManager on GetxController {
-  final _mainError = RxString('');
-  Stream<String?> get mainErrorStream => _mainError.stream;
-  set mainError(String value) => _mainError.value = value;
+  final _mainError = Rx<UiError?>(null);
+  Stream<UiError?> get mainErrorStream => _mainError.stream;
+  set mainError(UiError? value) => _mainError.subject.add(value);
 }
