3af02c9681feb538afbfd2284e9f33948c19370c
joaonotfound <joaonotfound@protonmail.com>
2023-03-25 00:04:28 -0300

    feat: initiliaze spring project with kotlin

---
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/EntrepotyApplication.kt
@@ -0,0 +1,11 @@
+package com.entrepoty.Entrepoty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EntrepotyApplication
+
+fun main(args: Array<String>) {
+	runApplication<EntrepotyApplication>(*args)
+}
---
+++ packages/server/src/test/kotlin/com/entrepoty/Entrepoty/EntrepotyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.entrepoty.Entrepoty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EntrepotyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
