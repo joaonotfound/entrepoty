a0c4cd339f148b3bb0ad29381a4915ce369e8c66
joaonotfound <joaonotfound@protonmail.com>
2023-03-26 17:26:11 -0300

    feat: create User entity

---
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/entities/User.kt
@@ -0,0 +1,29 @@
+package com.entrepoty.Entrepoty.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.jetbrains.annotations.NotNull
+
+@Entity
+class User {
+    @field:Id
+    @field:GeneratedValue(strategy = GenerationType.AUTO)
+    var id: String = ""
+
+    @field:Column(unique = true)
+    @field:NotNull
+    var username: String = ""
+
+    @field:NotNull
+    var password: String = ""
+
+    // 0 = readonly
+    // 1 = employer
+    // 2 = administrator
+    @field:NotNull
+    var permissionLevel: Short = 0;
+}
