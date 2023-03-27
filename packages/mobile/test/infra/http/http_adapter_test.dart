3644f614978a9717cfb68d832761a6711a4795f2
joaonotfound <joaonotfound@protonmail.com>
2023-03-27 17:27:58 -0300

    test: should call client with correct values

--- packages/mobile/test/infra/http/http_adapter_test.dart
+++ packages/mobile/test/infra/http/http_adapter_test.dart
@@ -12,7 +12,8 @@ class HttpAdapter {
     required this.client,
   });
 
-  Future<HttpResponse> get({required String url}) async {
+  @override
+  Future<HttpResponse<T>> get<T>({required String url}) async {
     try {
       var response = await client.get(Uri.parse(url));
       return HttpResponse(
@@ -23,6 +24,11 @@ class HttpAdapter {
       return const HttpResponse(statuscode: 500);
     }
   }
+
+  @override
+  Future<void> post<T>({required String url, Map? body}) async {
+    await client.post(Uri.parse(url), body: body);
+  }
 }
 
 class ClientMock extends Mock implements http.Client {
@@ -30,6 +36,11 @@ class ClientMock extends Mock implements http.Client {
   void mockGet(http.Response response) =>
       mockGetCall().thenAnswer((invocation) async => response);
   void mockGetThrows() => mockGetCall().thenThrow(Error());
+
+  When mockPostCall() => when(() => post(any(), body: any(named: "body")));
+  void mockPost(http.Response response) =>
+      mockPostCall().thenAnswer((invocation) async => response);
+  void mockPostThrows() => mockPostCall().thenThrow(Error());
 }
 
 void main() {
@@ -41,7 +52,7 @@ void main() {
   });
   setUp(() {
     client = ClientMock();
-    client.mockGet(http.Response("{}", 200));
+    client.mockPost(http.Response("{}", 200));
     sut = HttpAdapter(client: client);
   });
 
@@ -52,14 +63,14 @@ void main() {
       verify(() => client.get(Uri.parse(url))).called(1);
     });
     test("should return correct status code", () async {
-      client.mockGet(http.Response("{}", 300));
+      client.mockPost(http.Response("{}", 300));
 
       var response = await sut.get(url: url);
 
       expect(response.statuscode, 300);
     });
     test("should return correct body", () async {
-      client.mockGet(http.Response("{ \"username\": \"any-username\" }", 200));
+      client.mockPost(http.Response("{ \"username\": \"any-username\" }", 200));
 
       var response = await sut.get(url: url);
 
@@ -67,11 +78,19 @@ void main() {
       expect(response.statuscode, 200);
     });
     test("should return 500 error if client throws", () async {
-      client.mockGetThrows();
+      client.mockPostThrows();
 
       var response = await sut.get(url: url);
 
       expect(response.statuscode, 500);
+    });
+  });
+  group("HttpAdapter post", () {
+    test("should call client with correct values", () async {
+      await sut.post(url: url, body: {"key": "value"});
+
+      verify(() => client.post(Uri.parse(url), body: {"key": "value"}))
+          .called(1);
     });
   });
 }
