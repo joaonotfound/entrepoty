39adb99272114d07432759e5a5ae82395bff1786
joaonotfound <joaonotfound@protonmail.com>
2023-03-26 21:27:34 -0300

    test: should call saveSecureStorage with correct values

---
+++ packages/mobile/test/data/usecases/logout/local_logout_test.dart
@@ -0,0 +1,24 @@
+import 'package:entrepoty/data/data.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../mocks/save_secure_cache_storage_mock.dart';
+
+void main() {
+  late LocalLogout sut;
+  late MockSaveSecureCacheStorage storage;
+
+  setUp(() {
+    storage = MockSaveSecureCacheStorage();
+    storage.mockSaveSecure(null);
+    sut = LocalLogout(storage: storage);
+  });
+
+  group("LocalLogout", () {
+    test("should call saveSecureStorage with correct values", () {
+      sut.logout();
+
+      verify(() => storage.saveSecure(key: "token", value: "")).called(1);
+    });
+  });
+}
