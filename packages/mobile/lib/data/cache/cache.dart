97649f2bcf925317d928527aab7b985372edaa5d
joaonotfound <joaonotfound@protonmail.com>
2023-03-26 22:59:59 -0300

    feat: create SecureCacheStorage

--- packages/mobile/lib/data/cache/cache.dart
+++ packages/mobile/lib/data/cache/cache.dart
@@ -1,3 +1,4 @@
 export './cache_storage.dart';
 export './save_secure_cache_storage.dart';
 export './load_secure_cache_storage.dart';
+export './secure_cache_storage.dart';
---
+++ packages/mobile/lib/data/cache/secure_cache_storage.dart
@@ -0,0 +1,4 @@
+import 'package:entrepoty/data/cache/cache.dart';
+
+abstract class SecureCacheStorage
+    implements LoadSecureCacheStorage, SaveSecureCacheStorage {}
