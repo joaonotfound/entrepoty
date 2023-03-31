c91627f3814e0222419e1a1fd3fd32b8bca185f6
joaonotfound <joaonotfound@protonmail.com>
2023-03-31 18:35:43 -0300

    fix: fix wrong type

--- packages/server/src/main/kotlin/com/entrepoty/Entrepoty/repositories/UserRepository.kt
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/repositories/UserRepository.kt
@@ -3,4 +3,4 @@ package com.entrepoty.Entrepoty.repositories
 import com.entrepoty.Entrepoty.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, String>{}
+interface UserRepository : JpaRepository<User, Long>{}
