f5f30539cd6cd7a964758a7318126bdc49e754b4
joaonotfound <joaonotfound@protonmail.com>
2023-04-01 19:34:28 -0300

    feat: create LocalSignup class

--- packages/mobile/lib/data/usecases/authentication/authentication.dart
+++ packages/mobile/lib/data/usecases/authentication/authentication.dart
@@ -2,3 +2,4 @@ export 'remote_login.dart';
 export '../save_current_account/local_save_current_account.dart';
 export 'remote_login.dart';
 export 'local_login.dart';
+export 'local_signup.dart';
---
+++ packages/mobile/lib/data/usecases/authentication/local_signup.dart
@@ -0,0 +1,24 @@
+import 'package:entrepoty/domain/domain.dart';
+import 'package:fpdart/src/either.dart';
+
+class LocalSignup implements SignupUsecase {
+  @override
+  Future<Either<Account, DomainError>> register({
+    required String name,
+    required String username,
+    required String password,
+  }) async {
+    await Future.delayed(Duration(seconds: 2));
+    if (username == "ja12345") {
+      return Right(DomainError.invalidCredentials);
+    }
+    return Left(
+      Account(
+        token: "random-token",
+        username: username,
+        name: "João",
+        profilePictureUrl: "https://google.com",
+      ),
+    );
+  }
+}
