425b59b4986b06cad6ca1a4e456de682ab9d39c9
joaonotfound <joaonotfound@protonmail.com>
2023-04-01 20:26:56 -0300

    feat: create SignupPresenter abstract class

--- packages/mobile/lib/ui/screens/signup/signup_presenter.dart
+++ packages/mobile/lib/ui/screens/signup/signup_presenter.dart
@@ -1,1 +1,19 @@
-abstract class SignupPresenter {}
+import '../../misc/misc.dart';
+
+abstract class SignupPresenter {
+  Stream<String?> get nameErrorStream;
+  Stream<String?> get usernameErrorStream;
+  Stream<String?> get passwordErrorStream;
+  Stream<String?> get navigateToStream;
+  Stream<bool> get isFormValidStream;
+  Stream<bool> get isLoadingStream;
+  Stream<UiError?> get mainErrorStream;
+
+  void validateName(String name);
+  void validateUsername(String username);
+  void validatePassword(String password);
+
+  Future<void> signup();
+
+  void dispose();
+}
