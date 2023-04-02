fe387e8006e52bbec128c4c7bd0a40b2628435a0
joaonotfound <joaonotfound@protonmail.com>
2023-04-02 15:39:46 -0300

    fix: duplicated slashes on url

--- packages/mobile/lib/main/backend_urls.dart
+++ packages/mobile/lib/main/backend_urls.dart
@@ -1,7 +1,7 @@
 class BackendUrls {
   static final production = false;
-  static final _productionUrl = "http://google.com:3000/";
-  static final _developmentUrl = "http://10.0.2.2:8080/";
+  static final _productionUrl = "http://google.com:3000";
+  static final _developmentUrl = "http://10.0.2.2:8080";
 
   static String _complete(String url) {
     return (production ? _productionUrl : _developmentUrl) + url;
