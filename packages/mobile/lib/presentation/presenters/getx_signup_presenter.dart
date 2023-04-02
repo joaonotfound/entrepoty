97475e8c7eafeef723054a23e9d5ed65470abf2c
joaonotfound <joaonotfound@protonmail.com>
2023-04-01 21:03:01 -0300

    feat: should emit invalid when validator fails

--- packages/mobile/lib/presentation/presenters/getx_signup_presenter.dart
+++ packages/mobile/lib/presentation/presenters/getx_signup_presenter.dart
@@ -16,12 +16,15 @@ class GetxSignupPresenter extends GetxController
     required this.validation,
   });
 
+  String _name = "";
   final _nameError = RxString('');
   Stream<String?> get nameErrorStream => _nameError.stream;
 
+  String _username = "";
   final _usernameError = RxString('');
   Stream<String?> get usernameErrorStream => _usernameError.stream;
 
+  String _password = "";
   final _passwordError = RxString('');
   Stream<String?> get passwordErrorStream => _passwordError.stream;
 
@@ -31,20 +34,35 @@ class GetxSignupPresenter extends GetxController
     throw UnimplementedError();
   }
 
+  void _validateForm() {
+    isFormValid = _nameError.value == '' &&
+        _name != '' &&
+        _usernameError.value == '' &&
+        _username != '' &&
+        _passwordError.value == '' &&
+        _password != '';
+  }
+
   @override
   void validateName(String name) {
+    _name = name;
     _nameError.value = validation.validate(field: "name", value: name) ?? "";
+    _validateForm();
   }
 
   @override
   void validatePassword(String password) {
+    _password = password;
     _passwordError.value =
         validation.validate(field: "password", value: password) ?? "";
+    _validateForm();
   }
 
   @override
   void validateUsername(String username) {
+    _username = username;
     _usernameError.value =
         validation.validate(field: "username", value: username) ?? "";
+    _validateForm();
   }
 }
