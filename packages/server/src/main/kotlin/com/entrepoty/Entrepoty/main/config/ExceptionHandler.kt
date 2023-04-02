0d1baf944b88b439272d4e24dcb3976b4d108de6
joaonotfound <joaonotfound@protonmail.com>
2023-04-02 19:28:10 -0300

    feat: handle missing param on requests

---
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/main/config/ExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.entrepoty.Entrepoty.main.config
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class ExceptionHandler() : ResponseEntityExceptionHandler() {
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        return ResponseEntity.badRequest().build();
+    }
+}
