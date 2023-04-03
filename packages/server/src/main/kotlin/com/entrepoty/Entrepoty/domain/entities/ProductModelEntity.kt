c2c55d106a06e0c7189433c222e33b99f3a7285d
joaonotfound <joaonotfound@protonmail.com>
2023-04-03 13:08:01 -0300

    feat: update ProductModelEntity

--- packages/server/src/main/kotlin/com/entrepoty/Entrepoty/domain/entities/ProductModelEntity.kt
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/domain/entities/ProductModelEntity.kt
@@ -1,10 +1,14 @@
 package com.entrepoty.Entrepoty.domain.entities
 
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.jetbrains.annotations.NotNull
 
+@Entity
+@Table(name = "product_models")
 class ProductModelEntity{
     @field:NotNull
     @field:Id
--- packages/server/src/main/kotlin/com/entrepoty/Entrepoty/presentation/controllers/ProductModelController.kt
+++ packages/server/src/main/kotlin/com/entrepoty/Entrepoty/presentation/controllers/ProductModelController.kt
@@ -5,6 +5,7 @@ import com.entrepoty.Entrepoty.domain.entities.ProductModelEntity
 import com.entrepoty.Entrepoty.presentation.helpers.ResponseUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -25,6 +26,7 @@ class ProductModelController  {
             { model -> ResponseEntity.ok(model) }
         );
     }
+    @GetMapping
     fun loadModels(): ResponseEntity<List<ProductModelEntity>> {
         return service.loadModels().fold(
             { error -> utils.fromDomain(error) },
