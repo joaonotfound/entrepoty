d963f8ecdec3cb53a853972a3f2d959f07d1a534
joaonotfound <joaonotfound@protonmail.com>
2023-04-04 20:15:10 -0300

    feat: create ImagePickerImpl class

---
+++ packages/mobile/lib/infra/imagepicker/imagepicker.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:entrepoty/domain/domain.dart';
+import 'package:fpdart/src/either.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerImpl implements TakeImageUsecase {
+  final imagePicker = ImagePicker();
+  Future<Either<DomainError, String>> takeImage() async {
+    try {
+      final image = await imagePicker.pickImage(source: ImageSource.camera);
+      if(image != null){
+        return Either.right(image.path);
+      }
+    }catch (e) {}
+    return Either.left(DomainError.unexpected);
+  }
+}
--- packages/mobile/lib/infra/infra.dart
+++ packages/mobile/lib/infra/infra.dart
@@ -1,3 +1,4 @@
 export 'cache/cache.dart';
 export 'theme/theme.dart';
 export 'http/http.dart';
+export 'imagepicker/imagepicker.dart';
