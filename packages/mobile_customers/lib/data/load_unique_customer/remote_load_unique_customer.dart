d8450930e437e60b71c73c712725d3e8ab920776
joaonotfound <joaonotfound@protonmail.com>
2023-04-07 20:36:44 -0300

    feat: implement RemoteLoadUniqueCustomer class

--- packages/mobile_customers/lib/data/load_unique_customer/remote_load_unique_customer.dart
+++ packages/mobile_customers/lib/data/load_unique_customer/remote_load_unique_customer.dart
@@ -1,13 +1,29 @@
-
 import 'package:fpdart/fpdart.dart';
 import 'package:mobile_core/domain/errors/domain_errors.dart';
 import 'package:mobile_customers/domain/domain.dart';
+import 'package:mobile_remote/domain/domain.dart';
 
 class RemoteLoadUniqueCustomer implements LoadUniqueCustomerUsecase {
-  @override
-  Future<Either<DomainError, CustomerEntity>> loadUniqueCustomer(String enrollment) {
-    // TODO: implement loadUniqueCustomer
-    throw UnimplementedError();
+  HttpClient httpClient;
+  String url;
+
+  RemoteLoadUniqueCustomer({
+    required this.httpClient,
+    required this.url,
+  });
+
+  Future<Either<DomainError, CustomerEntity>> loadUniqueCustomer(
+    String enrollment,
+  ) async {
+    try {
+      final response = await httpClient.get(url: "$url/$enrollment");
+      if(response.statuscode == 200){
+        return Either.right(CustomerEntity.fromJson(response.body));
+      }
+      if(response.statuscode == 409){
+        return Either.left(DomainError.accountDoesntExist);
+      }
+    }catch (e){}
+    return Either.left(DomainError.unexpected);
   }
-  
 }
--- packages/mobile_customers/lib/main/factories/load_unique_customer_usecase.dart
+++ packages/mobile_customers/lib/main/factories/load_unique_customer_usecase.dart
@@ -1,5 +1,10 @@
+import 'package:mobile_core/backend_urls.dart';
 import 'package:mobile_customers/data/data.dart';
+import 'package:mobile_remote/main/http_factory.dart';
 
 RemoteLoadUniqueCustomer makeRemoteLoadUniqueCustomer() {
-  return RemoteLoadUniqueCustomer();
+  return RemoteLoadUniqueCustomer(
+    httpClient: makeHttpAdapter(),
+    url: BackendUrls.customers,
+  );
 }
