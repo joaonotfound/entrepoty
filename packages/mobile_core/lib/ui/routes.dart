4c11129dbc761d208b45ec3871468aa5a9a98c9e
joaonotfound <joaonotfound@protonmail.com>
2023-04-07 20:38:39 -0300

    feat: update Routes clas

--- packages/mobile_core/lib/ui/routes.dart
+++ packages/mobile_core/lib/ui/routes.dart
@@ -1,21 +1,24 @@
 class Routes {
-  static final splash = "/splash";
-  static final launch = '/launch';
-  static final signup = "/signup";
-  static final login = '/login';
-  static final home = '/home';
-  static final stock = "/stock";
-  static final settings = '/settings';
-  static final createCustomer = '/customers/create';
-  static final createStockItem = '/stock/create';
-  static final createModel = '/models/create';
+  static const splash = "/splash";
+  static const launch = '/launch';
+  static const signup = "/signup";
+  static const login = '/login';
+  static const home = '/home';
+  static const stock = "/stock";
+  static const settings = '/settings';
+  static const createCustomer = '/customers/create';
+  static const createStockItem = '/stock/create';
+  static const createModel = '/models/create';
 
-  static final viewModel = "/models/view/:id";
+  static const viewModel = "/models/view/:id";
   static getViewModel(int id) => "/models/view/$id";
 
-  static final viewBorrow = "/borrow/view/:id";
+  static const viewBorrow = "/borrow/view/:id";
   static getViewBorrow(int id) => "/borrow/view/$id";
 
-  static final createBorrow = '/borrow/create';
+  static const viewCustomer = '/customers/view/:enrollment';
+  static getViewCustomer(String enrollment) => "/customers/view/$enrollment";
+
+  static const createBorrow = '/borrow/create';
 
 }
