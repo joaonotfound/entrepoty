980d701d75aaafb92ce5e718163675743f7cf013
joaonotfound <joaonotfound@protonmail.com>
2023-04-10 02:06:43 -0300

    fix: missing files

---
+++ packages/mobile_borrow/lib/domain/entities/employer_entity.dart
@@ -0,0 +1,11 @@
+class Employer {
+  String username;
+  String role;
+
+  Employer({
+    required this.username,
+    required this.role,
+  });
+
+  factory Employer.fromJson(Map json) => Employer(username: json['username'] ?? "", role: json['username'] ?? '');
+}
