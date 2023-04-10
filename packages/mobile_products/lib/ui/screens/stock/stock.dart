9b35fcb57e48774618ccaf08a00a904615d0c7ef
joaonotfound <joaonotfound@protonmail.com>
2023-04-10 02:48:55 -0300

    fix: create StockViewProductScreen

--- packages/mobile_products/lib/ui/screens/stock/stock.dart
+++ packages/mobile_products/lib/ui/screens/stock/stock.dart
@@ -1,2 +1,3 @@
 export 'stock_list/stock_list.dart';
 export 'stock_item_creation/stock_item_creation.dart';
+export 'stock_view_product/stock_view_product.dart';
---
+++ packages/mobile_products/lib/ui/screens/stock/stock_view_product/stock_view_product.dart
@@ -0,0 +1,2 @@
+export 'stock_view_product_presenter.dart';
+export 'stock_view_product_screen.dart';
