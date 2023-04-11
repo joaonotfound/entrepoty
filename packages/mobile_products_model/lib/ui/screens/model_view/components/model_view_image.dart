e05b77e997d4de1f25fd1e368c4594b5523f41f4
joaonotfound <joaonotfound@protonmail.com>
2023-04-11 01:34:24 -0300

    feat: handle error while showing image

--- packages/mobile_products_model/lib/ui/screens/model_view/components/model_view_image.dart
+++ packages/mobile_products_model/lib/ui/screens/model_view/components/model_view_image.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_core/mobile_core.dart';
@@ -18,16 +19,26 @@ class ModelViewImage extends StatelessWidget {
     return Expanded(
       flex: 1,
       child: FutureBuilder(
-        initialData: "",
-        future: backend.loadResource(model.imagePath),
-        builder: (context, snapshot) {
-          return Image.network(
-            snapshot.data!,
-            fit: BoxFit.cover,
-            width: MediaQuery.of(context).size.width,
-          );
-        }
-      ),
+          initialData: "",
+          future: backend.loadResource(model.imagePath),
+          builder: (context, snapshot) {
+            return snapshot.data == null
+                ? Container(
+                    color: Theme.of(context).colorScheme.surface,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Icon(FluentIcons.image_16_regular),
+                  )
+                : Image.network(
+                    snapshot.data!,
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
+                    errorBuilder: (_, __, ___) => Container(
+                      color: Theme.of(context).colorScheme.surface,
+                      width: MediaQuery.of(context).size.width,
+                      child: const Icon(FluentIcons.image_16_regular),
+                    ),
+                  );
+          }),
     );
   }
 }
