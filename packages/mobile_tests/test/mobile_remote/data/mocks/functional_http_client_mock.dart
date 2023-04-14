4acdb4ee9f2a36dc325d87fc894486ec89aa37bb
joaonotfound <joaonotfound@protonmail.com>
2023-04-14 00:13:13 -0300

    test: create MockFunctionalHttpClient

---
+++ packages/mobile_tests/test/mobile_remote/data/mocks/functional_http_client_mock.dart
@@ -0,0 +1,24 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:mobile_remote/mobile_remote.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFunctionalHttpClient extends Mock implements FunctionalHttpClientUsecase {
+  // ignore: unnecessary_this
+  When _mockGetCall() => when(
+          () => get(url: any(named: "url"), timeout: any(named: "timeout")));
+  void mockGet(Either<HttpErrorEnum, HttpResponse> response) =>
+      // ignore: prefer_const_constructors
+  _mockGetCall().thenAnswer((_) async => response);
+  void mockGetError(HttpError error) => _mockGetCall().thenThrow(error);
+
+  When _mockPostCall() =>
+      // ignore: unnecessary_this
+  when(() => this.post(
+      url: any(named: "url"),
+      body: any(named: "body"),
+      timeout: any(named: "timeout")));
+  void mockPost(Either<HttpErrorEnum, HttpResponse> response) =>
+      // ignore: prefer_const_constructors
+  _mockPostCall().thenAnswer((_) async => response);
+  void mockPostError(HttpError error) => _mockPostCall().thenThrow(error);
+}
