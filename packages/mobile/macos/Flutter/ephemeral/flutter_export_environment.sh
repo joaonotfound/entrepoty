902b6de364fbe1460b324ca97db446c2e2290136
joaonotfound <joaonotfound@protonmail.com>
2024-12-14 11:45:22 -0300

    build: updated dependencies to run the projectqq

---
+++ packages/mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/nix/store/5llkahyq1p04wm2j260gzba69nkqy2xb-flutter-wrapped-3.24.4-sdk-links"
+export "FLUTTER_APPLICATION_PATH=/home/joaonotfound/personal/entrepoty/packages/mobile"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
